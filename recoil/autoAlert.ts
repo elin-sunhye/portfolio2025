c31de18da228e35d782ca947c8fd09bd62eb30bf
elin-sunhye <tjsgp43@gmail.com>
2024-12-26 15:07:39 +0900

    auto alert 컴포넌트 추가

--- recoil/autoAlert.ts
+++ recoil/autoAlert.ts
@@ -1,21 +1,21 @@
-import { StatusType } from "@/type/common/commonType";
-import { atom } from "recoil";
+import {atom} from "recoil";
+import {StatusType} from "@/type/common/commonType";
 
 // 화면에 autoAlert을 띄울지 말지를 결정하는 true/false atom recoilstate
 export const openAlertState = atom({
-  key: "openAlertState",
-  default: false,
+    key: "openAlertState",
+    default: false,
 });
 
 // autoAlert에 어떤 텍스트를 띄울지를 결정하는 string atom recoilstate
 export const openAlertText = atom({
-  key: "openAlertText",
-  default: "",
+    key: "openAlertText",
+    default: "",
 });
 
 // autoAlert이 어떤 성격(출력 색깔)을 가질지를 결정하는 string atom recoilstate
 // success, warning, info, error 4가지의 type을 가진다.
 export const openAlertStatus = atom<StatusType>({
-  key: "openAlertStatus",
-  default: "success",
+    key: "openAlertStatus",
+    default: "success",
 });
