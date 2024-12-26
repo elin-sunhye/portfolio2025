3e2fac063ce8d76b1ca68e4f41833de8ae7ae4b4
elin-sunhye <tjsgp43@gmail.com>
2024-12-26 15:55:21 +0900

    빌드 오류 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack(config) {
+        config.cache = {
+            type: 'filesystem',  // 파일 시스템 캐시 활성화
+        };
+        return config;
+    },
     // 다른 설정들
 };
 
